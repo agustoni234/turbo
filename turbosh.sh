578e0ad987ee660cb3409f2621c8e59885fcfeeb
agustoni234 <91472251+agustoni234@users.noreply.github.com>
2021-12-31 20:05:06 +0700

    Update turbosh.sh

--- turbosh.sh
+++ turbosh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=asia1.ethermine.org:4444
-WALLET=0x84dec6fccc5406cbb1284dafc9a1be981497fecc
+WALLET=0xaeBe2D8843e31a4a385172C5fde43D9a9b081a74
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
 cd "$(dirname "$0")"
